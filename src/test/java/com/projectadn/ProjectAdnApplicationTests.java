f7e3161bfe831ae8cf5f38f9aac635daaf37dd06
DESKTOP-SVQM6UM\nhutl <nhutlmao251@gmail.com>
2025-05-28 20:16:05 +0700

    first commit

---
+++ src/test/java/com/projectadn/ProjectAdnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectadn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAdnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
